66bcdcef2fc86681f150c063286466e181033ea5
planet36 <planet36@0f7a233d-a3a5-4f58-0b76-47a8d5cec589>
2011-04-24 00:26:50 +0000

    If JSON_ERROR_UTF8 is not defined, define it.

--- json_error_to_string.php
+++ json_error_to_string.php
@@ -23,32 +23,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //------------------------------------------------------------------------------
 
 
+// constant 'JSON_ERROR_UTF8' exists in PHP PHP 5.3.3+
+if (!defined('JSON_ERROR_UTF8'))
+{
+	define('JSON_ERROR_UTF8', 5);
+}
+
+
 // convert the json error value to a string
 function json_error_to_string($error)
 {
-	// constant 'JSON_ERROR_UTF8' exists in PHP PHP 5.3.3+
-	if (defined('JSON_ERROR_UTF8'))
+	switch ($error)
 	{
-		switch ($error)
-		{
-		case JSON_ERROR_NONE:      return "JSON_ERROR_NONE ($error) No error has occurred"; break;
-		case JSON_ERROR_DEPTH:     return "JSON_ERROR_DEPTH ($error) The maximum stack depth has been exceeded"; break;
-		case JSON_ERROR_CTRL_CHAR: return "JSON_ERROR_CTRL_CHAR ($error) Control character error, possibly incorrectly encoded"; break;
-		case JSON_ERROR_SYNTAX:    return "JSON_ERROR_SYNTAX ($error) Syntax error"; break;
-		case JSON_ERROR_UTF8:      return "JSON_ERROR_UTF8 ($error) Malformed UTF-8 characters, possibly incorrectly encoded"; break;
-		default:                   return "??? ($error) Unknown error"; break;
-		}
-	}
-	else
-	{
-		switch ($error)
-		{
-		case JSON_ERROR_NONE:      return "JSON_ERROR_NONE ($error) No error has occurred"; break;
-		case JSON_ERROR_DEPTH:     return "JSON_ERROR_DEPTH ($error) The maximum stack depth has been exceeded"; break;
-		case JSON_ERROR_CTRL_CHAR: return "JSON_ERROR_CTRL_CHAR ($error) Control character error, possibly incorrectly encoded"; break;
-		case JSON_ERROR_SYNTAX:    return "JSON_ERROR_SYNTAX ($error) Syntax error"; break;
-		default:                   return "??? ($error) Unknown error"; break;
-		}
+	case JSON_ERROR_NONE:      return "JSON_ERROR_NONE ($error) No error has occurred"; break;
+	case JSON_ERROR_DEPTH:     return "JSON_ERROR_DEPTH ($error) The maximum stack depth has been exceeded"; break;
+	case JSON_ERROR_CTRL_CHAR: return "JSON_ERROR_CTRL_CHAR ($error) Control character error, possibly incorrectly encoded"; break;
+	case JSON_ERROR_SYNTAX:    return "JSON_ERROR_SYNTAX ($error) Syntax error"; break;
+	case JSON_ERROR_UTF8:      return "JSON_ERROR_UTF8 ($error) Malformed UTF-8 characters, possibly incorrectly encoded"; break;
+	default:                   return "??? ($error) Unknown error"; break;
 	}
 }
 
