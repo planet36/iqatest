1a582c252776c8e54dab97f61e2c2716af26294e
planet36 <planet36@users.noreply.github.com>
2021-12-07 21:22:40 -0500

    Use SPDX tags

--- json_error_to_string.php
+++ json_error_to_string.php
@@ -1,23 +1,7 @@
 <?php
 
-/*
-
-Image Quality Assessment Test
-Copyright (C) 2013 Steve Ward
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, version 3 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
 
 // constant 'JSON_ERROR_UTF8' exists in PHP 5.3.3+
 if (!defined('JSON_ERROR_UTF8'))
--- submit-results.php
+++ submit-results.php
@@ -1,23 +1,7 @@
 <?php
 
-/*
-
-Image Quality Assessment Test
-Copyright (C) 2015 Steve Ward
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, version 3 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
 
 // https://pear.php.net/package/Mail_Mime/
 require_once('Mail/mime.php');
@@ -60,20 +44,8 @@ if (!array_key_exists('results', $_POST))
 
 <!--
 
-Image Quality Assessment Test
-Copyright (C) 2015 Steve Ward
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, version 3 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+SPDX-FileCopyrightText: Steven Ward
+SPDX-License-Identifier: OSL-3.0
 
 -->
 
